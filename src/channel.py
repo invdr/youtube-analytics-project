7053e9b844896e25bcd0eaaf46bd5911a2c5219f
Ibrahim WORK <work@mail.ri>
2023-08-03 14:38:41 +0300

    add methods for class Channel

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,23 @@
+import json
+import os
+
+from googleapiclient.discovery import build
+
+API_KEY: str = os.getenv('YT_API_KEY')
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    """Класс для ютуб-канала."""
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        """Экземпляр инициализируется id канала. Дальше все данные будут
+        подтягиваться по API."""
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        youtube = build('youtube', 'v3', developerKey=API_KEY)
+        channel = youtube.channels().list(id=self.channel_id,
+                                          part='snippet,statistics').execute()
+        printj = json.dumps(channel, indent=2, ensure_ascii=False)
+        print(printj)
