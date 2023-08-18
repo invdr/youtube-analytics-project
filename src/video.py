cbc7b20e72e3f946c2098b20c78f32082a079c34
Ноутбук AQ <aq@mail.ru>
2023-08-18 19:22:48 +0300

    add video.py

---
+++ src/video.py
@@ -0,0 +1,32 @@
+import json
+
+from src.channel import Channel
+
+# ввод id канала для извлечения данных видео
+CHANNEL_ID = 'UC-OVMPlMA3-YCIeg4z5z23A'
+
+
+class Video(Channel):
+    def __init__(self, video_id, channel_id=CHANNEL_ID):
+        super().__init__(channel_id)
+        self.__video_id = video_id
+
+        yt_obj = self.get_service()
+        video_response = yt_obj.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                              id=video_id).execute()
+        self.video_title = video_response['items'][0]['snippet']['title']
+        self.video_url = 'https://www.youtube.com/watch?v=' + self.__video_id
+        self.view_count = video_response['items'][0]['statistics']['viewCount']
+        self.like_count = video_response['items'][0]['statistics']['likeCount']
+
+    def __str__(self):
+        return self.video_title
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id, channel_id=CHANNEL_ID):
+        super().__init__(video_id, channel_id=CHANNEL_ID)
+        self.playlist_id = playlist_id
+
+    def __str__(self):
+        return self.video_title
